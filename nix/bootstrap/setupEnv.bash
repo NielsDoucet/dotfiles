2acd699fe6930f21cdf2f5c9d8912f4da80cc10a
Niels Doucet <niels.doucet@collibra.com>
2017-01-04 09:05:33 +0100

    add gradle tab completion and start bootstrap script

---
+++ nix/bootstrap/setupEnv.bash
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# install homebrew
+
+# install basic brews
+# get them from brews.sh
+
+# install wanted casks
+# get them from casks.sh
+
+# copy bash profile files
+cp ../.bash_profile ~
+cp ../.bashrc ~
+cp ../.brew.bash ~
+cp ../.git-completion.bash
+cp ../.gradle-tab-completion.bash ~
