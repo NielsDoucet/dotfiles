4df19ce80b89489927fafa637e2c30d83360eb77
Niels Doucet <niels.doucet@collibra.com>
2017-03-21 12:26:23 +0100

    fix ordering

--- nix/bootstrap/setupHomebrew.bash
+++ nix/bootstrap/setupHomebrew.bash
@@ -1,22 +1,21 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install python3
-brew install git
-brew install bash-completion
-brew install bash-git-prompt
-brew install httpie
-brew install maven
-brew install mercurial
-brew install node
-brew install openssl
-brew install p7zip
-brew install tree
-brew install wget
-brew install yank
-
 brew cask install java
 brew cask install intellij-idea
 brew cask install gimp
 brew cask install virtualbox
 brew cask install vagrant
 brew cask install wireshark
+
+brew install node
+brew install git
+brew install python3
+brew install bash-completion
+brew install bash-git-prompt
+brew install httpie
+brew install maven
+brew install mercurial
+brew install p7zip
+brew install tree
+brew install wget
+brew install yank
