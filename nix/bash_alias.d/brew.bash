d7fd20feac55d39ebfc430439dfc5fa3dc605a32
Niels Doucet <niels.doucet@collibra.com>
2017-08-30 10:50:41 +0200

    use brew cask outdated and brew cask reinstall instead

--- nix/bash_alias.d/brew.bash
+++ nix/bash_alias.d/brew.bash
@@ -3,10 +3,3 @@ alias brewloc='brew --prefix'
 
 alias caskinfo='brew cask info'
 alias casklist='brew cask list'
-
-caskup() {
-  rm -rf "$(brew --cache)"
-  brew update
-  brew cask uninstall --force "$@"
-  brew cask install "$@"
-}
